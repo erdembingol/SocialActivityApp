53f2dbca64ed90fb606eb2f4f720ab2f4ced6c2e
Erdem Bing”l <erdembingol@hotmail.com>
2014-05-04 13:01:13 +0300

    Proje oluşturuldu ve anasayfa yapımına başlandı

---
+++ social_activity/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SocialActivity::Application.config.secret_key_base = '309d5cc8d21533f900391af757d3c0e3f9e5a223aabb28360206eff9f92dde4101b976270c8498e7e7f654deb867f55a74a7d9a035bd1c32809be66960fa2b6c'
